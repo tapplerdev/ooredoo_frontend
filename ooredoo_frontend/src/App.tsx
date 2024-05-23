0738988e9a490e0f08b42a29c9e8250e329c84f6
tapplerdev <151861568+tapplerdev@users.noreply.github.com>
2024-05-23 08:19:57 +0300

    tailwind
    tailwind

--- ooredoo_frontend/src/App.tsx
+++ ooredoo_frontend/src/App.tsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
+import Table from './components/Table';
 function App() {
-  const [count, setCount] = useState(0)
+  const [channels, setChannels] = useState([]);
+  const [editingChannel, setEditingChannel] = useState(null);
+  const [newChannelName, setNewChannelName] = useState('');
+  const [newChannelNumber, setNewChannelNumber] = useState('');
+  console.log(channels)
+  const fetchChannels = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/channels');
+      const data = await response.json();
+      setChannels(data);
+    } catch (error) {
+      console.error('Error fetching channels:', error);
+    }
+  };
+
+  const editChannel = (channel) => {
+    setEditingChannel(channel.id);
+    setNewChannelName(channel.channelName);
+    setNewChannelNumber(channel.channelNumber);
+  };
+
+  const updateChannel = async (channel) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/channels/change-channel/${channel.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ channelName: newChannelName, channelNumber: newChannelNumber }),
+      });
+      if (response.ok) {
+        const updatedChannel = await response.json();
+        setChannels(channels.map(ch => (ch.id === channel.id ? updatedChannel : ch)));
+        setEditingChannel(null);
+        setNewChannelName('');
+        setNewChannelNumber('');
+      } else {
+        console.error('Failed to update channel');
+      }
+    } catch (error) {
+      console.error('Error updating channel:', error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      {/* <h1>Retrieve Ooredoo Channels</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={fetchChannels}>
+          Fetch Channels
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.js</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <h2>Channels</h2>
+        <ul className="no-bullets">
+          {channels.map((channel) => (
+            <li key={channel.id}>
+              {editingChannel === channel.id ? (
+                <>
+                  <div>
+                    <label>
+                      Channel Number:
+                      <input
+                        type="number"
+                        value={newChannelNumber}
+                        onChange={(e) => setNewChannelNumber(e.target.value)}
+                      />
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      Channel Name:
+                      <input
+                        type="text"
+                        value={newChannelName}
+                        onChange={(e) => setNewChannelName(e.target.value)}
+                      />
+                    </label>
+                  </div>
+                  <button onClick={() => updateChannel(channel)}>Save</button>
+                  <button onClick={() => setEditingChannel(null)}>Cancel</button>
+                </>
+              ) : (
+                <>
+                  Channel ID: {channel.id} 
+                  | Channel number: {channel.channel_number} 
+                  | Package: {channel.package_name} 
+                  | Product ID: {channel.product_id}
+                  | Channel Name: {channel.channel_name}
+                  <button onClick={() => editChannel(channel)}>Edit</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Table />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ooredoo_frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./src/App.tsx", "./src/**/**/*.{jsx,tsx}", "./src/**/**/**/*.{jsx,tsx}", "./src/**/**/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
